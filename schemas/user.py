982c6a5f78c414589fa55a6784873e51c5358966
akotronis <akotronis@gmail.com>
2023-02-08 12:25:36 +0200

    Sorted fields for user schema

--- schemas/user.py
+++ schemas/user.py
@@ -2,6 +2,8 @@ from marshmallow import Schema, fields
 
 
 class UserSchema(Schema):
+    class Meta:
+        ordered = True  
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
     password = fields.Str(
